8249cfe21310e89601d67a5679467c893b36b2fd
Richard Olpindo <djricky088@yahoo.com>
2024-09-13 16:15:50 -1000

    Solved closest.ts algorithm

--- closest.ts
+++ closest.ts
@@ -1,6 +1,36 @@
-export function closest(nums: number[], target: number): number {
-   // Implement me
-   return 0;
+export function closest(nums: number[], target: number) {
+  // Implement me
+  function toPositive(number: number) {
+    return Math.sqrt(Math.pow(number, 2));
+  }
+
+  const sortedNums = nums.sort((a, b) => a - b);
+
+  let prevDifference = 1000000;
+
+  let closestSum = 0;
+  for (let i = 0; i < sortedNums.length; i++) {
+    let j = i + 1;
+    let k = sortedNums.length - 1;
+
+    while (j < k) {
+      let sum = sortedNums[i] + sortedNums[j] + sortedNums[k];
+
+      let newDifference = target - sum;
+
+      if (toPositive(newDifference) < toPositive(prevDifference)) {
+        prevDifference = newDifference;
+        closestSum = sum;
+      }
+
+      if (sum > target) {
+        k--;
+      } else {
+        j++;
+      }
+    }
+  }
+  return closestSum;
 }
 
-module.exports = { closest }
+module.exports = { closest };
